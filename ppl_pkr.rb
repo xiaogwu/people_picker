8c9229d4ff3e439c417c12cc6724243a53bc652e
Xiao G. Wu <xiaogwu@gmail.com>
2013-06-17 20:15:11 -0700

    Read in file and randomize method

--- ppl_pkr.rb
+++ ppl_pkr.rb
@@ -1,9 +1,24 @@
 #!/usr/bin/env ruby
 
 # TODO: 
-# 1. Read file into Array
+# [x] 1. Read file into Array
 # 2. Get random names from array based on -n flag
 # 3. Create n arrays based on -g flag
 # Bonus 1 - Store/retrieve array in a json file
 # Bonus 2 - History
+
+filename = 'Names.txt'
+names = []
+# Open filename in read mode and for each line push into array
+File.open(filename, 'r').each_line do |line|
+	names << line.strip
+end
+
+# Method to shuffle array in place
+def randomize(names_array)
+	names_array.shuffle!
+end
+
+puts names
+
 
