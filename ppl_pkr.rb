324309321a970acb44d70b58c1ebd245db0698e9
Xiao G. Wu <xiaogwu@gmail.com>
2013-06-17 19:39:36 -0700

    Initial commit

---
+++ ppl_pkr.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+# TODO: 
+# 1. Read file into Array
+# 2. Get random names from array based on -n flag
+# 3. Create n arrays based on -g flag
+# Bonus 1 - Store/retrieve array in a json file
+# Bonus 2 - History
+
