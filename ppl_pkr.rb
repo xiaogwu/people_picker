7d153d9957dcc91784337a733c7e0b6d14e5353d
Xiao G. Wu <xiaogwu@gmail.com>
2013-06-17 22:45:19 -0700

    Implemented group of n people flag option

--- ppl_pkr.rb
+++ ppl_pkr.rb
@@ -2,7 +2,7 @@
 
 # TODO: 
 # [x] 1. Read file into Array
-# 2. Get random names from array based on -n flag
+# [x] 2. Get random names from array based on -n flag
 # 3. Create n arrays based on -g flag
 # Bonus 1 - Store/retrieve array in a json file
 # Bonus 2 - History
@@ -27,10 +27,19 @@ when '-n' then
 	puts "Number of people specified exceed class size" if (ARGV[1].to_i - 1) > names.length
 	puts names[0..ARGV[1].to_i - 1] unless (ARGV[1].to_i - 1) > names.length
 when '-g' then
-	# Group flag selected"
-	puts ARGV[1].to_i
+	# Group of n flag selected"
+	randomize(names)
+	size_of_group = ARGV[1].to_i
+	group_id = 1
+	until names.size == 0 do
+		puts "Group " + group_id.to_s
+		puts "================="
+		puts names.shift(size_of_group)
+		puts
+		group_id += 1
+	end	
 else
-	puts "No flag provided"
+	puts "No flag provided, provide usage"
 end
 
 #puts names
