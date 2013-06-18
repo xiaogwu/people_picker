ee9b8431777e7718dd664a14bf9c46fdb677694f
Xiao G. Wu <xiaogwu@gmail.com>
2013-06-17 21:04:49 -0700

    Implemented n number of random people flag option

--- ppl_pkr.rb
+++ ppl_pkr.rb
@@ -19,6 +19,20 @@ def randomize(names_array)
 	names_array.shuffle!
 end
 
-puts names
+# flag logic
+case ARGV[0]
+when '-n' then
+	# Random number flag selected
+	randomize(names)
+	puts "Number of people specified exceed class size" if (ARGV[1].to_i - 1) > names.length
+	puts names[0..ARGV[1].to_i - 1] unless (ARGV[1].to_i - 1) > names.length
+when '-g' then
+	# Group flag selected"
+	puts ARGV[1].to_i
+else
+	puts "No flag provided"
+end
+
+#puts names
 
 
